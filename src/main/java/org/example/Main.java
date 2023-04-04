d8b77ef0fae4a99ee8fdf47ff0fa9b855d46416b
Kuandyk <aytzhankuandyk@gmail.com>
2023-04-04 16:27:55 +0600

    QA ABLE

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        mainMethodshowMessage();
+        mainMethodShowInfo();
+    }
+    private static void mainMethodshowMessage() {
+        System.out.println("Hello my friend\n");
+    }
+    private static void mainMethodShowInfo() {
+        System.out.println("Hello Kuandyk");
+        }
+    }
+
